e5fedd1af16c3edfbdfb553f2e59c1fe0c159e7d
Axel <axel.agvald@gmail.com>
2025-04-24 14:40:44 +0200

    edge-klassen skapad

--- backend/src/main/java/se/su/inlupp/Edge.java
+++ backend/src/main/java/se/su/inlupp/Edge.java
@@ -1,12 +1,30 @@
 package se.su.inlupp;
 
-public interface Edge<T> {
+public class Edge<T> {
 
-  int getWeight();
+  private T destination;
+  private int weight;
+  private String name;
 
-  void setWeight(int weight);
+  public Edge(T destination, int weight, String name) {
+    this.destination = destination;
+    this.weight = weight;
+    this.name = name;
+  }
 
-  T getDestination();
+  int getWeight() {
+    return weight;
+  }
 
-  String getName();
+  void setWeight(int weight) {
+    this.weight = weight;
+  }
+
+  T getDestination() {
+    return destination;
+  }
+
+  String getName() {
+    return name;
+  }
 }
